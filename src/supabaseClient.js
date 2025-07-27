8624c93f43fd9777ca2b0de509a1916b6aa9a6e0
Aaliyakhan10 <aaliyakhan4352@gmail.com>
2025-07-27 21:27:14 +0530

    hide credential

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fhwvkbjaollnvppmplfo.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZod3ZrYmphb2xsbnZwcG1wbGZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MDcwNzgsImV4cCI6MjA2OTE4MzA3OH0.jzTfONIaYAPEaX5N_M0iZ3yDDy2Cyq_lkRXeczF1xho'; // Get this from your Supabase dashboard (Project > API > anon key)
+const supabaseUrl =`${import.meta.env.VITE_SUPABASE_URL}`;
+const supabaseKey = `${import.meta.env.VITE_SUPABASE_API_KEY}`; // Get this from your Supabase dashboard (Project > API > anon key)
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
